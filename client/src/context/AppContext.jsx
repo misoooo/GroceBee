2759c88c1e2a8e5c8ae116934e9df833624cdd95
Mimansa <mimansa0107@gmail.com>
2025-04-08 02:39:31 +0530

    bestseller section added

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,13 +1,70 @@
-import { Children, createContext, useContext, useState } from "react";
+import {
+  Children,
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
 import { useNavigate } from "react-router-dom";
+import { dummyProducts } from "../assets/assets";
+import toast from "react-hot-toast";
 
 export const AppContext = createContext();
 
 export const AppContextProvider = ({ children }) => {
+  const currency = import.meta.VITE_CURRENCY;
+
   const navigate = useNavigate();
+
   const [user, setUser] = useState(null);
   const [isSeller, setIsSeller] = useState(false);
   const [showUserLogin, setShowUserLogin] = useState(false);
+  const [products, setProducts] = useState([]);
+  const [cartItems, setCartItems] = useState([]);
+
+  //fetch all products
+  const fetchproducts = async () => {
+    setProducts(dummyProducts);
+  };
+
+  //add products to cart
+  const addToCart = (itemId) => {
+    let cartdata = structuredClone(cartItems);
+    if (cartdata[itemId]) {
+      cartdata[itemId] += 1;
+    } else {
+      cartdata[itemId] = 1;
+    }
+    setCartItems(cartdata);
+    toast.success("Added to Cart");
+  };
+
+  //update cart quantity
+
+  const updateCartItem = (itemId, quantity) => {
+    let cartdata = structuredClone(cartItems);
+    cartdata[itemId] = quantity;
+    setCartItems(cartdata);
+    toast.success("Cart Updated");
+  };
+
+  //remove product from cart
+
+  const removeFromCart = (itemId) => {
+    let cartdata = structuredClone(cartItems);
+    if (cartdata[itemId]) {
+      cartdata[itemId] -= 1;
+      if (cartdata[itemId] === 0) {
+        delete cartdata[itemId];
+      }
+    }
+    toast.success("Removed From Cart");
+    setCartItems(cartdata);
+  };
+
+  useEffect(() => {
+    fetchproducts();
+  }, []);
 
   const value = {
     navigate,
@@ -17,6 +74,12 @@ export const AppContextProvider = ({ children }) => {
     setIsSeller,
     showUserLogin,
     setShowUserLogin,
+    products,
+    currency,
+    addToCart,
+    updateCartItem,
+    removeFromCart,
+    cartItems
   };
 
   return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
