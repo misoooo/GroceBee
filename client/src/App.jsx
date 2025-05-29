860331e77907b3777508ad5477a02c50315e0183
Mimansa <mimansa0107@gmail.com>
2025-05-29 18:53:06 +0530

    minor fixes done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,16 +4,30 @@ import { Routes, Route, useLocation  } from "react-router-dom";
 import Home from './pages/Home';
 import { Toaster } from "react-hot-toast";
 import Footer from "./components/Footer";
+import { useAppContext } from "./context/AppContext";
+import Login from "./components/Login";
+import AllProducts from "./pages/AllProducts";
+import ProductCategory from "./pages/ProductCategory";
+import ProductDetails from "./pages/productDetails";
+import Cart from "./pages/Cart";
+import AddAddress from "./pages/AddAddress";
 
 const App = () => {
   const isSellerPath = useLocation().pathname.includes("seller");
+  const {showUserLogin} = useAppContext();
   return (
     <div>
       {isSellerPath ? null : <Navbar />}
+      {showUserLogin ? <Login/>: null}
       <Toaster/>
       <div className={isSellerPath ? "" : "px-6 md:px-16 lg:px-24 xl:px-32"}>
         <Routes>
           <Route path='/' element={<Home/>} />
+          <Route path='/products' element={<AllProducts/>} />
+          <Route path='/products/:category' element={<ProductCategory/>} />
+          <Route path='/products/:category/:id' element={<ProductDetails/>} />
+          <Route path='/cart' element={<Cart/>} />
+          <Route path='/add-address' element={<AddAddress/>} />
         </Routes>
       </div>
       {!isSellerPath && <Footer/>}
--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -20,7 +20,8 @@ export const AppContextProvider = ({ children }) => {
   const [isSeller, setIsSeller] = useState(false);
   const [showUserLogin, setShowUserLogin] = useState(false);
   const [products, setProducts] = useState([]);
-  const [cartItems, setCartItems] = useState([]);
+  const [cartItems, setCartItems] = useState({});
+  const [searchQuery, setSearchQuery] = useState({});
 
   //fetch all products
   const fetchproducts = async () => {
@@ -62,6 +63,27 @@ export const AppContextProvider = ({ children }) => {
     setCartItems(cartdata);
   };
 
+  //get cart item count
+  const getCartCount = () =>{
+    let totalCount = 0;
+    for(const item in cartItems){
+      totalCount+=cartItems[item]
+    }
+    return totalCount;
+  }
+
+  //get cart total amount
+  const getCartAmount = () =>{
+    let totalAmount = 0;
+    for(const items in cartItems){
+      let itemInfo = products.find((product)=>product._id===items);
+      if(cartItems[items]>0){
+        totalAmount += itemInfo.offerPrice * cartItems[items]
+      }
+    }
+    return Math.floor(totalAmount*100)/100;
+  }
+
   useEffect(() => {
     fetchproducts();
   }, []);
@@ -79,7 +101,11 @@ export const AppContextProvider = ({ children }) => {
     addToCart,
     updateCartItem,
     removeFromCart,
-    cartItems
+    cartItems,
+    searchQuery,
+    setSearchQuery,
+    getCartCount,
+    getCartAmount
   };
 
   return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
